f2cbc8a948031b24bb50538dc9b18e196f69b46c
jajaroyana505 <muhammadjajaroyana3@gmail.com>
2024-07-13 19:53:05 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\MerchantController;
 use Illuminate\Support\Facades\Auth;
@@ -17,8 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+// Route::get('/', [LoginController::class, 'index']);
 
 Auth::routes();
 
